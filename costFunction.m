319ae9c8acb533c7d0ba828e22eb9cb3a3ceae60
Suraj Pulloor <thegamer600@gmail.com>
2018-12-02 03:38:12 +0530

    Vectorized Cost Function, Gradient. Vectorized the whole project. No loops used.

--- costFunction.m
+++ costFunction.m
@@ -1,9 +1,16 @@
 function [jVal, grad] = costFunction(theta, X, y, lambda)
     
+    % Re-shape theta
+    theta = reshape(theta, 14, 3);
+
     m = size(X, 1);
 
-    jVal = -1/m * (y' * log(sigmoid(X * theta)) + (1-y)' * log(1 - sigmoid(X * theta))) + lambda/(2*m) * sum(theta .^ 2);
+    jVal = -1/m * (y .* log(sigmoid(X * theta)) + (1-y) .* log(1 - sigmoid(X * theta)));
+    jVal = sum(sum(jVal)) + lambda/(2*m) * sum(sum(theta .^ 2));
 
     grad = 1/m * (X' * (sigmoid(X * theta) - y));
 
-    grad = [grad(1); grad(2:end) + lambda/m * (theta(2:end))];
+    grad = [grad(:, 1) grad(:, 2:end) + lambda/m * (theta(:, 2:end))];
+
+    % Un-roll gradients for fminunc
+    grad = grad(:);
--- gd.m
+++ gd.m
@@ -1,3 +1,8 @@
 function optTheta = gd(theta, X, y, lambda)
+    % Un-roll parameters for fminunc
+    theta = theta(:);
+    % We are providing the gradient's and iteration count and run fminunc
     options = optimset('GradObj', 'on', 'MaxIter', 600);
     optTheta = fminunc(@(t)(costFunction(t, X, y, lambda)), theta, options);
+    % Resize output Theta
+    optTheta = reshape(optTheta, 14, 3);
