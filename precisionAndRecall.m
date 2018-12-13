d7648fd93147f2be4df869d031674c9351628d91
Suraj Pulloor <thegamer600@gmail.com>
2018-12-13 21:07:04 +0530

    Precision & Recall Implemenation corrected. Removed wrong implementation. Uses a loop approach.

--- precisionAndRecall.m
+++ precisionAndRecall.m
@@ -10,48 +10,45 @@ function [precision, recall] = precisionAndRecall(h_x_indices, y_test)
     precision = 0;
     recall = 0;
 
-    % printf('\n\n**********Computing Precision & Recall***********\n\n');
+    printf('\n\n**********Computing Precision & Recall***********\n\n');
 
     for i = 1:length(classes),
 
-        % printf('%d) Precision of h_x for class=%d\n', i, classes(i));
-        % printf('i.e class=%d is treated as positive class, and the other two classes are treated as negative classes\n', classes(i));
+        printf('%d) Precision of h_x for class=%d\n', i, classes(i));
+        printf('i.e class=%d is treated as positive class, and the other two classes are treated as negative classes\n', classes(i));
 
         % Convert both y_test labels and h_x_indices into positive and negative classes i.e 0's and 1's
         y_test_for_class = y_test == classes(i);
         h_x_indices_for_class = h_x_indices == classes(i);
 
-        % Find the no. of 1's and 0's for the y label
-        y_test_1s = y_test_for_class == 1;
-        y_test_0s = y_test_for_class == 0;
+        truePositive = 0;
+        falsePositive = 0;
+        falseNegative = 0;
 
-        % Find the no. of 1's and 0's for the h_x_indices label
-        h_x_indices_1s = h_x_indices_for_class == 1;
-        h_x_indices_0s = h_x_indices_for_class == 0;
+        for j = 1:length(y_test_for_class),
+            if y_test_for_class(j) == 1 && h_x_indices_for_class(j) == 1,
+                truePositive += 1;
+            elseif y_test_for_class(j) == 0 && h_x_indices_for_class(j) == 1,
+                falsePositive += 1;
+            elseif y_test_for_class(j) == 1 && h_x_indices_for_class(j) == 0,
+                falseNegative += 1;
+            end;
 
-
-        % True Positive
-        truePositive = abs(length(find(h_x_indices_1s)) - length(find(y_test_1s)));
-
-        % False Positive
-        falsePositive = abs(length(find(h_x_indices_1s)) - length(find(y_test_0s)));
-
-        % False Negative
-        falseNegative = abs(length(find(h_x_indices_0s)) - length(find(y_test_1s)));
+        end;
 
         % Precision
-        precision += truePositive / (truePositive + falsePositive);
+        precision = truePositive / (truePositive + falsePositive);
 
         % Recall
-        recall += truePositive / (truePositive + falseNegative);
+        recall = truePositive / (truePositive + falseNegative);
 
         
 
         % Print the values
-        % printf('\n**For class=%d**,\n**Precision=%d, Recall=%d**\n\n\n', classes(i), precision, recall);
+        printf('\n**For class=%d**,\n**Precision=%d, Recall=%d**\n\n\n', classes(i), precision, recall);
 
-        % pause;
+        pause;
     end;
 
-    % printf('*********End*********\n\n');
+    printf('*********End*********\n\n');
 end
